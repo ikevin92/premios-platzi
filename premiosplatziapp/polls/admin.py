1e5b59c24d6897ce0d1b19db5963f3bba10cb705
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-11-15 20:25:48 -0500

    admin

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,8 +1,22 @@
+from typing import Sequence
 from django.contrib import admin
 from .models import Question, Choice
-# Register your models here.
 
 
-admin.site.register(Question)
+# class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields: Sequence[str] = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
